3ae52ad9d1adc490a573e606d6fddc5344aa8cc8
takagi <iam.takagi.dev@gmail.com>
2022-01-02 15:58:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# CommandBlocker
+コマンドを無効化するプラグイン
+
+OP権限プレイヤー もしくは パーミッションノード (`commandblocker.ignore`) を所持しているプレイヤーは `config.yml` で設定したコマンドの実行が無効化されます。
+
+## ビルド / How to build
+[![Build](https://github.com/iamtakagi/CommandBlocker/actions/workflows/build.yml/badge.svg)](https://github.com/iamtakagi/CommandBlocker/actions/workflows/build.yml)
+
+`mvn clean package`
+
+## ライセンス / LICENCE
+MIT.
---
+++ src/main/java/com/github/iamtakagi/commandblocker/CommandBlocker.java
@@ -0,0 +1,44 @@
+package com.github.iamtakagi.commandblocker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CommandBlocker extends JavaPlugin implements Listener {
+
+	private List<String> blockedCommands = new ArrayList<String>();
+	private String denyMessage = ChatColor.RESET + "Unknown command. Type \"/help\" for help.";
+
+	@Override
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(this, this);
+		this.saveDefaultConfig();
+		this.blockedCommands = this.getConfig().getStringList("settings.blocked_commands");
+		this.denyMessage = ChatColor.RESET
+				+ ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("settings.deny_message"));
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onCommandProcess(PlayerCommandPreprocessEvent event) {
+		Player player = event.getPlayer();
+		if (player.isOp() || player.hasPermission("commandblocker.ignore"))
+			return;
+
+		String message = (event.getMessage().startsWith("/") ? "" : "/") + event.getMessage();
+
+		for (String blockedCommand : blockedCommands) {
+			if (message.startsWith(blockedCommand)) {
+				player.sendMessage(this.denyMessage);
+				event.setCancelled(true);
+				return;
+			}
+		}
+	}
+}
